ff51bb217494efa2122ec204f163e56ee778ef63
SergeyRyskin <sergeyryskin54@gmail.com>
2023-05-27 18:48:20 +0300

    fix

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -3,7 +3,7 @@ export const appInfo = {
     // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
     appName: "Diktatorial",
     apiDomain: "http://localhost:3000",/*if you do a LocalDev cahnge to "http://localhost:3000" */
-    websiteDomain: "http://localhost:3000",/*if you do a LocalDev cahnge to "http://localhost:3000" */
+    websiteDomain: "diktatorial-web.vercel.app",/*if you do a LocalDev cahnge to "http://localhost:3000" */
     apiBasePath: "/api/auth",
     websiteBasePath: "/upload"
   }
